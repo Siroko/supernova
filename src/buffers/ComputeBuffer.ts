83afc69e2ecea82082add8284870c479ab25c0c9
Felix Martinez <sirokosan@gmail.com>
2024-10-14 14:06:30 +0200

    updated signature

--- src/buffers/ComputeBuffer.ts
+++ src/buffers/ComputeBuffer.ts
@@ -1,11 +1,20 @@
 import { BufferBase } from "./BufferBase";
 
+interface IComputeBufferOptions {
+    type?: string;
+    usage: GPUFlagsConstant;
+    buffer?: Float32Array;
+}
+
 class ComputeBuffer extends BufferBase {
 
     public type?: string = ComputeBuffer.BUFFER_TYPE_STORAGE;
 
-    constructor() {
+    constructor(options: IComputeBufferOptions) {
         super();
+        this.type = options.type;
+        this.usage = options.usage;
+        this.buffer = options.buffer;
     }
 
     public setBuffer(buffer: Float32Array, usage: GPUFlagsConstant = GPUBufferUsage.STORAGE) {
